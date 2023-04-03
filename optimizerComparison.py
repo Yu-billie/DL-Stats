db4431e73eda769b4775ec618fa5aa721228b3b6
Yumin Kim <110521952+Yu-billie@users.noreply.github.com>
2023-04-04 03:22:54 +0900

    Update optimizerComparison.py

--- optimizerComparison.py
+++ optimizerComparison.py
@@ -44,31 +44,21 @@ x_test /= 255
 """## 3. Model Building"""
 
 batch_size=64
-
 num_classes=10
-
 epochs=10
 
 def build_model(optimizer):
-
     model=Sequential()
-
     model.add(Conv2D(32,kernel_size=(3,3),activation='relu',input_shape=input_shape))
-
     model.add(MaxPooling2D(pool_size=(2,2)))
-
     model.add(Dropout(0.25))
-
     model.add(Flatten())
-
+    
     model.add(Dense(256, activation='relu'))
-
     model.add(Dropout(0.5))
-
     model.add(Dense(num_classes, activation='softmax'))
-
+    
     model.compile(loss=keras.losses.categorical_crossentropy, optimizer= optimizer, metrics=['accuracy'])
-
     return model
 
 """## 4. Model Training
